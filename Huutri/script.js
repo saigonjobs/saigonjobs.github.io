397516d04f4e66a2456030ec7f83ea5ef26cf207
thuutri2710 <34308662+thuutri2710@users.noreply.github.com>
2019-05-11 21:20:32 +0700

    update func

--- Huutri/script.js
+++ Huutri/script.js
@@ -1,12 +1,12 @@
 function render(data){
-	var html="<div class='comentBox'><div class='leftPanelImg'><img src='logo.png'/></div><div class='rightPanel'><span>"+data.name+"</span><div class='date'>"+data.date+"</div><p>"+data.body+"<br/></p></div><div class='clear'></div></div>";
+	var html="<div class='comentBox'><div class='leftPanelImg'><img src='logo.png'/></div><div class='rightPanel'><span>"+data.name+"</span><div class='date'>"+data.date+"</div><div class='rate'>"+data.rate+"</div><p>"+data.body+"<br/></p></div><div class='clear'></div></div>";
 	$('#container').append(html);
 }
 
 
 
 $(document).ready(function(){
-	var coment=[{"name":"tri","date":"2/3/2019","body":"boyd"}];
+	var coment=[];
     if(!localStorage.comentData){
     	localStorage.comentData=[];
     }
@@ -17,9 +17,21 @@ $(document).ready(function(){
         render(coment[i]);
     }
     $('#addComent').click(function(){
+    	var today=new Date();
+    	var d = today.getUTCDate()+1;
+    	var m = today.getUTCMonth()+1;
+    	var y = today.getUTCFullYear();
+    	var r = $('#rate').val();
+    	var rate='';
+    	for(var i=0;i<r;i++){
+    		rate=rate+"&#9733";
+    	}
+
+    	var time=d+"/"+m+"/"+y;
     	var addObj = {
            "name": $('#name').val(),
-           "date": $('#date').val(),
+           "date": time,
+           "rate": rate,
            "body": $('#bodyText').val()
         };
        console.log(addObj);
@@ -27,6 +39,7 @@ $(document).ready(function(){
        render(addObj);
        $('#name').val('');
        $('#date').val('dd/mm/yyyy');
+       $('#rate').val('');
        $('#bodyText').val('');
     });
 });
