0cf7ea1bee8248ef6d12e63096c08977e8c874fd
Hai Dang <haidangc23hn2@gmail.com>
2019-05-16 23:54:04 +0700

    server, db, search function, convert ejs

--- dang/searchRoute.js
+++ dang/searchRoute.js
@@ -1,9 +1,12 @@
 const search = require('./searchFunction.js');
 
 module.exports = function(app) {
-    app.route('/data').get(function(req, res) {
-        var text = req.query.text;
-        var data = search(text, res);
-        //res.send(data)
+    app.route('/').get(function(req, res) {
+        // var text = req.query.text;
+        // var data = search(text, res);
+        // //res.send(data)
+        console.log("Route");
+        var searchText = req.query.searchText;
+        search(searchText, res);
     })
 }
