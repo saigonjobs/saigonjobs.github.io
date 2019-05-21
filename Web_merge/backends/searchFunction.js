be624e317a118612e6c233476982e7dfb556e54a
Hai Dang <haidangc23hn2@gmail.com>
2019-05-21 20:51:08 +0700

    fix close database, add apply button

--- Web_merge/backends/searchFunction.js
+++ Web_merge/backends/searchFunction.js
@@ -12,12 +12,12 @@ app.use(session({
 }));
 
 job_recruitment = mongoose.model("job_recruitment", schemas.job_recruitment_schema);
-
+var user_info = mongoose.model("user_info", schemas.user_info_schema);
 
 module.exports = function search(text, req, res) {
     var personal = [req.session.reg_num, req.session.role];
     if ((text === '') || (text == undefined)) {
-        res.render('index', {result: [], personal: personal});
+        res.render('index', { result: [], personal: personal });
     } else {
         var tagList = text.split(" ");
         for (var i = tagList.length; i >= 0; i--) {
@@ -25,13 +25,23 @@ module.exports = function search(text, req, res) {
                 tagList.splice(tagList.indexOf(tagList[i]), 1);
         }
 
+        if (personal[0] !== undefined) {
+            user_info.find({ _id: req.session.reg_num }).lean().exec(function (err, user) {
+                if (user[0].cart.length != 0) {
+                    for (var i = 0; i < user[0].cart.length; i++) {
+                        personal.push(user[0].cart[i].job_id);
+                    }
+                }
+            });
+        }
+
         var url = "mongodb://localhost:27017/database";
-        mongoose.connect(url, function(err, db) {
+        mongoose.connect(url, function (err, db) {
             if (err) throw err;
-           job_recruitment.find({job_tags: {$in: tagList} }).lean().exec(function(err, result) {
+            job_recruitment.find({ job_tags: { $in: tagList } }).lean().exec(function (err, result) {
                 if (err) throw err;
-                res.render('index',{result: result, company: [], personal: personal, detail: false});
-                mongoose.connection.close();
+                res.render('index', { result: result, company: [], personal: personal, detail: false });
+                //mongoose.connection.close();
             });
         });
     }
--- Web_merge/backends/searchID.js
+++ Web_merge/backends/searchID.js
@@ -13,20 +13,30 @@ app.use(session({
 
 job_recruitment = mongoose.model("job_recruitment", schemas.job_recruitment_schema);
 company_info = mongoose.model("company_info", schemas.company_info_schema);
+var user_info = mongoose.model("user_info", schemas.user_info_schema);
 
-module.exports = function searchID(id, req, res) {  
+module.exports = function searchID(id, req, res) {
     var personal = [req.session.reg_num, req.session.role];
     if ((id === '') || (id == undefined)) {
-        res.render('index', {result: [], personal: personal});
+        res.render('index', { result: [], personal: personal });
     } else {
         var url = "mongodb://localhost:27017/database";
-        mongoose.connect(url, function(err, db) {
+        mongoose.connect(url, function (err, db) {
             if (err) throw err;
-            job_recruitment.find({_id: id}).lean().exec(function(err, result) {
+            if (personal[0] !== undefined) {
+                user_info.find({ _id: req.session.reg_num }).lean().exec(function (err, user) {
+                    if (user[0].cart.length != 0) {
+                        for (var i = 0; i < user[0].cart.length; i++) {
+                            personal.push(user[0].cart[i].job_id);
+                        }
+                    }
+                });
+            }
+            job_recruitment.find({ _id: id }).lean().exec(function (err, result) {
                 if (err) throw err;
-                company_info.find({_id: result[0].company_id}).lean().exec(function(err, docs){
-                    res.render('index',{result: result, company: docs, personal: personal, detail: true});
-                    mongoose.connection.close();
+                company_info.find({ _id: result[0].company_id }).lean().exec(function (err, docs) {
+                    res.render('index', { result: result, company: docs, personal: personal, detail: true });
+                    //mongoose.connection.close();
                 });
             });
         });
